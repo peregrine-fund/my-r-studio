e6daea1c7ef93381b9a1be5d190b6d84f557d76d
sde32kl <96574995@fsv.cuni.cz>
2026-04-27 19:41:42 +0200

    Implement data visualization and analysis for production data
    Added data visualization and statistical analysis for industrial production data, including time series graphs, summary statistics, and outlier detection.

--- main.R
+++ main.R
@@ -1,3 +1,45 @@
 # 1. Load Data
 industrial_production = read.csv("fed-non-durable.csv")
+industrial_production$observation_date <- as.Date(industrial_production$observation_date, format = "%Y-%m-%d")
 
+library(ggplot2)
+library(dplyr)
+#time series graph for production volumes
+ggplot(industrial_production, aes(x = observation_date, y = IPG326S)) +
+  geom_line() +
+  scale_x_date(
+    date_breaks = '3 years',
+    date_labels = '%Y'
+  ) +
+  theme_minimal() +
+  theme(
+    axis.text.x = element_text(angle = 45, hjust = 1)
+  )
+
+#time series graph for production volumes growth rates
+ggplot(industrial_production, aes(x = observation_date, y = IPG326S_CH1)) +
+  geom_line() +
+  scale_x_date(
+    date_breaks = "3 years",
+    date_labels = "%Y"
+  ) +
+  theme_minimal() +
+  theme(
+    axis.text.x = element_text(angle = 45, hjust = 1)
+  )
+
+#median, mean, max, min for growth rates
+print(median(industrial_production$IPG326S_CH1, na.rm = TRUE))
+print(mean(industrial_production$IPG326S_CH1, na.rm = TRUE))
+max_growth <- industrial_production[which.max(industrial_production$IPG326S_CH1), ]
+min_growth <- industrial_production[which.min(industrial_production$IPG326S_CH1), ]
+print(max_growth)
+print(min_growth)
+#boxplot for growth rates
+ggplot(data = industrial_production, aes(x = IPG326S_CH1)) +
+  geom_boxplot()
+
+#finding outliers using zscore
+industrial_production$growth_zscore <- (industrial_production$IPG326S_CH1 - mean(industrial_production$IPG326S_CH1, na.rm = TRUE))/sd(industrial_production$IPG326S_CH1, na.rm=TRUE)
+outliers_z <- industrial_production[abs(industrial_production$growth_zscore) > 3, ]
+print(outliers_z)
